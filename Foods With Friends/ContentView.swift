87ad02d8a6444f467074912d1f0e9eab28c215bd
Ella Speer-Zisook <s015721@students.lmsd.org>
2023-03-01 09:19:22 -0500

    Initial Commit

---
+++ Foods With Friends/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Foods With Friends
+//
+//  Created by Speer-Zisook, Ella on 3/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Foods With Friends/Foods_With_FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Foods_With_FriendsApp.swift
+//  Foods With Friends
+//
+//  Created by Speer-Zisook, Ella on 3/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Foods_With_FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
