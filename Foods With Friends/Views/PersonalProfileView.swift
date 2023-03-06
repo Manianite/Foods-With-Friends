3f9fac9e74f03ee3420041d1a83b36ac8ec2138c
Julia Zorc <s016756@students.lmsd.org>
2023-03-06 08:32:02 -0500

    start of profile view

--- Foods With Friends/Views/PersonalProfileView.swift
+++ Foods With Friends/Views/PersonalProfileView.swift
@@ -9,11 +9,74 @@ import SwiftUI
 
 struct PersonalProfileView: View {
     var body: some View {
-        HStack{
+        VStack{
+            HStack{
+                
+                VStack{
+                    //profile pic
+                    Image("profileDefault")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                    //    .frame(width: (UIScreen.width)/4)
+                        .clipShape(Circle())
+
+                    
+                    //location
+                    Text("Wynnewood, PA")
+                        .font(Constants.textFont)
+                    
+                    //bio
+                    Text("Hi! I am Julia and, just like you, I love food! I post reviews at least once a week. Be my friend to see my opinions and be a better informed foodie :)")
+                        .font(Constants.textFont)
+                    
+                }
+                
+                Spacer()
+                
+                VStack{
+                    HStack{
+                        //Name
+                        Text("Julia Zorc")
+                            .font(Constants.titleFont)
+                        
+                        Spacer()
+                        
+                        //username
+                        Text("@juliazorc123")
+                            .font(Constants.textFont)
+                            .foregroundColor(Color.highlight)
+                        
+                    }
+                    
+                    //# of reviews
+                    VStack{
+                        Text("123")
+                            .font(Constants.titleFont)
+                        
+                        Text("Reviews")
+                            .font(Constants.textFont)
+                        
+                    }
+                    
+                    //# of friends
+                    VStack{
+                        Text("78")
+                            .font(Constants.titleFont)
+                        
+                        Text("Friends")
+                            .font(Constants.textFont)
+                        
+                    }
+                    
+                }
+            }
             
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
+            Spacer()
+            
         }
     }
+    
 }
 
 struct PersonalProfileView_Previews: PreviewProvider {
