82b2a066083d020b1509df6560316beba2e41946
Julia Zorc <s016756@students.lmsd.org>
2023-03-08 12:19:15 -0500

    updates

--- Foods With Friends/Views/PersonalProfileView.swift
+++ Foods With Friends/Views/PersonalProfileView.swift
@@ -12,32 +12,33 @@ struct PersonalProfileView: View {
         VStack{
             HStack(){
                 Spacer()
-            
+                
             }
             
             HStack{
                 
                 VStack(alignment: .leading){
                     HStack(alignment: .bottom){
-                    
-                    //profile pic
-                    Image("profileDefault")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 100, height: 100)
-                        .clipShape(Circle())
-                        .padding(.leading)
                         
-                       
+                        //profile pic
+                        Image("profileDefault")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 100, height: 100)
+                            .clipShape(Circle())
+                            .padding(.leading)
+                        
+                        
                         
                         Button(){
                             //edit profile
                         } label: {
-                            Image(systemName: "pencil")
-                                .foregroundColor(Color.black)
+                            Image(systemName: "pencil.circle.fill")
+                                .foregroundColor(Color.highlight)
                                 .font(.system(size: 30))
                             
-                        }.padding(0.25)
+                        }.padding([.trailing, .top, .bottom], 0.25)
+                            .padding(.leading, -15.0)
                         
                         Spacer()
                         
@@ -72,25 +73,33 @@ struct PersonalProfileView: View {
                         Text("Julia Zorc")
                             .font(Constants.titleFont)
                             .padding(.leading)
-                            .padding([.top, .bottom, .leading], 0.25)
+                            .padding([.top, .bottom, .trailing], 0.25)
                         //username
                         Text("@juliazorc123")
                             .font(Constants.textFont)
-                            .foregroundColor(Color.highlight)
+                            .foregroundColor(Color.gray)
                             .padding(0.25)
                     }
-                    //Location
-                    Text("Wynnewood")
-                        .font(Constants.textFont)
-                        .padding(.leading)
-                        .padding([.top, .bottom, .leading], 0.25)
+                    
+                    HStack{
+                        Image(systemName: "mappin.and.ellipse")
+                            .foregroundColor(Color.highlight)
+                            .font(.system(size: 30))
+                            .padding([.top, .bottom], 0.25)
+                            .padding(.leading)
+                        
+                        //Location
+                        Text("Wynnewood")
+                            .font(Constants.textFont)
+                            .padding([.top, .bottom, .trailing], 0.25)
+                    }.padding(.leading, 1.0)
                     
                     //bio
                     Text("Hi! I am Julia and, just like you, I love food! I post reviews at least once a week. Be my friend to see my opinions and be a better informed foodie :)")
                         .font(Constants.textFontSmall)
                         .frame(width: 200)
                         .padding(.leading)
-                        .padding([.top, .bottom, .leading], 0.25)
+                        .padding([.top, .bottom, .trailing], 0.25)
                     
                 }
                 
